a95d72055a59f31481bcb14644cc270953235797
DualityOfMan <ianmleary@gmail.com>
2023-09-17 11:32:19 +0100

    final

--- PNG_ATTEMPT.py
+++ PNG_ATTEMPT.py
@@ -2,9 +2,12 @@
 
 
 Opng = open("pngwing.com.png","rb")
-for i in range (8):
-    print(int.from_bytes(Opng.read(1),'little'),end=" ")#header is correct
+Opng.read(8)#header
 #this code is dependent on information from the 'PNG Chunk File Reader' linked in the Log, and online converters
+#the following values are known:
+#Color type: RGBA with 8 bits per channel (theoretically 32 bits per pixel)
 Opng.read(8)#includes four seemingly unimportant bytes and the header name IHDR
 pWidth = int.from_bytes(Opng.read(4),'big')
+print(pWidth)
 pHeight = int.from_bytes(Opng.read(4),'big')
+
